e7688eae0b5d666599762396432b54391e9ded7e
Marian1718 <42773398+Marian1718@users.noreply.github.com>
2021-11-17 10:52:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var name = prompt("Please enter your name")
+
+var response = prompt(" Hello, " + name + "! Are you recruter?");
+if (response.toLowerCase() == "yes") {
+    alert("If you want to contact me write to https://t.me/marik7780. Have a nice day!");
+}
+else {
+    alert("Have a nice day then!")
+}
+var check = confirm("I hope, you will not judge stricly!")
